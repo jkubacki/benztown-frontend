2fa4d8b9a15be55a76c1f2ad0c825acd2bd3c854
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-23 14:54:38 -0500

    Configure basic setup for redux and router

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory, createMemoryHistory } from 'history';
+import { reducer as formReducer } from 'redux-form';
+
+const history = isWindowUndefined() ? createMemoryHistory() : createBrowserHistory();
+const historyMiddleware = routerMiddleware(history);
+
+export const reducers = combineReducers({
+  router: connectRouter(history),
+  form: formReducer,
+});
+
+const composeEnhancers =
+  getWindow().__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk, historyMiddleware)),
+);
+
+export { history };
