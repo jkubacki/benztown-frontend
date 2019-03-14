b8fc6bc2edabdedccde49d480b5a4b050127666d
Jakub Kubacki <kubacki.jk@gmail.com>
2019-03-13 18:10:00 -0600

    Add propTypes to LogoutButton

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { logout as logoutAction } from 'actions/auth';
@@ -12,6 +13,10 @@ function LogoutButton({ logout }) {
     </button>
   );
 }
+
+LogoutButton.propTypes = {
+  logout: PropTypes.func.isRequired,
+};
 
 export { LogoutButton };
 
