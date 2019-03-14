3e47fb2c2fa934ffd148b7c7a886ab0ea2c4d56e
Jakub Kubacki <kubacki.jk@gmail.com>
2019-03-13 19:26:39 -0600

    Fix lint issues in Element

--- src/components/Element.js
+++ src/components/Element.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactAudioPlayer from 'react-audio-player';
 
-class Element extends React.Component {
-  render() {
-    const { element } = this.props;
-    return (
-      <tr>
-        <td>
-          {element.attributes.name}
-        </td>
-        <td>
-          <ReactAudioPlayer
-            src={element.attributes.file}
-            controls
-          />
-        </td>
-        <td>
-          { element.attributes.tag_list.join(', ') }
-        </td>
-      </tr>
-    )
-  }
+function Element({ element }) {
+  return (
+    <tr>
+      <td>
+        {element.attributes.name}
+      </td>
+      <td>
+        <ReactAudioPlayer
+          src={element.attributes.file}
+          controls
+        />
+      </td>
+      <td>
+        { element.attributes.tag_list.join(', ') }
+      </td>
+    </tr>
+  );
 }
 
-export { Element };
+Element.propTypes = {
+  element: PropTypes.shape({
+    attributes: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      file: PropTypes.string.isRequired,
+      tag_list: PropTypes.array.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Element;
